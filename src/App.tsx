ac3575880cd78628a6ad26dc83e30e0e73203ca8
joooons <joon.s.chang@gmail.com>
2024-03-21 09:54:35 -0700

    add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>Home Care</div>
+      <Container></Container>
     </>
   );
 }
