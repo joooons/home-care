52da911841082d0208e71dcad27d4a481416d125
joooons <joon.s.chang@gmail.com>
2024-03-21 13:44:18 -0700

    init prelim hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import { Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Home Care</h1>
-      <Container></Container>
+      <div className='flex h-screen items-center justify-center bg-green-200 '>
+        <div className='container'>
+          <h1 className='text-3xl font-bold text-green-800 m-3'>Home Care</h1>
+          <p className='m-3'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum vel
+            repellendus necessitatibus delectus facere nisi dolore harum
+            voluptate blanditiis quos nam debitis, assumenda enim vero corrupti
+            ut mollitia a temporibus hic! Unde possimus cumque architecto dolore
+            eveniet perspiciatis sed laborum blanditiis sequi sunt repudiandae
+            cum eligendi natus repellendus repellat, labore non ab, ducimus
+            modi.
+          </p>
+          <button className='m-3'>Get Started</button>
+        </div>
+      </div>
     </>
   );
 }
