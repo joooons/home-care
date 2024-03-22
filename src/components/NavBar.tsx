7310deb7a8753c06c3d4358840abe87cd59d0762
joooons <joon.s.chang@gmail.com>
2024-03-21 21:59:50 -0700

    simplify navbar
    remove login and notification
    remove current selection visualization

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
-// import { Fragment } from 'react';
 import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
-  { name: 'Home Care', href: '#', current: true },
   { name: 'Mission', href: '#', current: false },
   { name: 'Location', href: '#', current: false },
   { name: 'Testimonials', href: '#', current: false },
+  { name: 'Location', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
 ];
 
 function classNames<T extends string | { [key: string]: boolean }>(
@@ -17,7 +17,7 @@ function classNames<T extends string | { [key: string]: boolean }>(
 
 export default function NavBar() {
   return (
-    <Disclosure as='nav' className='bg-gray-200'>
+    <Disclosure as='nav' className='bg-sky-200'>
       {({ open }) => (
         <>
           <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
@@ -41,7 +41,11 @@ export default function NavBar() {
                     src='./src/assets/home-alt-svgrepo-com.svg'
                     alt='Home Care'
                   />
+                  <div className='text-gray-800 px-4 py-3 text-xl font-medium font-serif'>
+                    HOME CARE
+                  </div>
                 </div>
+
                 <div className='hidden sm:ml-6 sm:block'>
                   <div className='flex space-x-4'>
                     {navigation.map((item) => (
@@ -49,10 +53,7 @@ export default function NavBar() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current
-                            ? 'text-blue-600'
-                            : 'text-gray-500 hover:bg-gray-400 hover:text-white',
-                          'rounded-md px-3 py-2 text-xl font-medium'
+                          'text-gray-800 hover:bg-cyan-300 hover:text-white rounded-md px-4 py-3 text-xl font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
@@ -61,11 +62,6 @@ export default function NavBar() {
                     ))}
                   </div>
                 </div>
-              </div>
-
-              <div className='absolute inset-y-0 right-0 text-xl font-medium flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-                {/* Profile dropdown */}
-                Contacts
               </div>
             </div>
           </div>
@@ -78,10 +74,7 @@ export default function NavBar() {
                   as='a'
                   href={item.href}
                   className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
+                    'text-gray-800 hover:bg-cyan-300 hover:text-white block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
                 >
