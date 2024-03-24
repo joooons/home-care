7819182f459e1f4c5b9a032239dbf10cccaea8ea
joooons <joon.s.chang@gmail.com>
2024-03-23 22:24:21 -0700

    use prettier-plugin-tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar"
 
-import './App.css';
+import "./App.css"
 
-import img_convo from './assets/convo.jpg';
+import img_convo from "./assets/convo.jpg"
 // import img_hug from './assets/hug.jpg';
 // import img_flower from './assets/flower.jpg';
-import img_chile from './assets/chile.jpg';
-import img_stare from './assets/stare.jpg';
-import img_house from './assets/house.jpg';
-
-import img_line_wave from './assets/line-wave.jpg';
+import img_chile from "./assets/chile.jpg"
+import img_stare from "./assets/stare.jpg"
+import img_house from "./assets/house.jpg"
+import img_line_wave from "./assets/line-wave.jpg"
 // import img_line_minim from './assets/line-minim.jpg';
 // import img_line_couch from './assets/line-couch.jpg';
-import img_abstract from './assets/abstract.jpg';
+import img_abstract from "./assets/abstract.jpg"
 
 function App() {
   return (
@@ -26,21 +25,24 @@ function App() {
           //  MM    MM  MM        MM    MM  MM    MM  
           //  MM    MM  MMMMMMMM  MM    MM    MMMM     */}
 
-      <section className='relative items-center justify-center -z-10'>
+      <section className="relative -z-10 items-center justify-center">
         <img
-          className='absolute inset-0 object-cover h-full w-full opacity-25 -z-10'
+          className="absolute inset-0 -z-10 h-full w-full object-cover opacity-25"
           src={img_house}
-          alt='image of a house'
+          alt="image of a house"
         />
-        <div className='flex flex-col items-center justify-center h-screen px-16 pt-24 pb-20'>
-          <h1 className='max-w-xl text-2xl lg:text-4xl text-center font-bold font-alegreya text-dark m-3'>
+        <div className="flex h-screen flex-col items-center justify-center px-16 pb-20 pt-24">
+          <h1 className="m-3 max-w-xl text-center font-alegreya text-2xl font-bold text-dark lg:text-4xl">
             We'll take care of your loved ones
           </h1>
-          <p className='text-center text-dark max-w-xl m-3'>
-          As a compassionate home care concierge service, we offer personalized care in the comfort of our clients' homes, focusing on daily activities, medication management, and companionship to support their independence and well-being.
+          <p className="m-3 max-w-xl text-center text-dark">
+            As a compassionate home care concierge service, we offer
+            personalized care in the comfort of our clients' homes, focusing on
+            daily activities, medication management, and companionship to
+            support their independence and well-being.
           </p>
-          <div className='flex justify-center'>
-            <button className='bg-sky-500 text-light'>LEARN MORE</button>
+          <div className="flex justify-center">
+            <button className="bg-sky-500 text-light">LEARN MORE</button>
           </div>
         </div>
       </section>
@@ -52,27 +54,27 @@ function App() {
           //    MM  MM      MM    MM    MM    MM    MM    MM  MM    MM  
           //      MM      MMMMMM    MMMM    MMMMMM    MMMM    MM    MM   */}
 
-      <section className='grid grid-cols-1 lg:grid-cols-2 items-center justify-center bg-gradient-to-r from-sky-400 from-5% via-sky-300 via-50% to-sky-200 to-95%'>
-        <div className='w-full md:w-auto overflow-hidden bg-white'>
-            <img
-              className='object-cover w-full h-full'
-              src={img_convo}
-              alt='image of a conversation'
-            />
+      <section className="grid grid-cols-1 items-center justify-center bg-gradient-to-r from-sky-400 from-5% via-sky-300 via-50% to-sky-200 to-95% lg:grid-cols-2">
+        <div className="w-full overflow-hidden bg-white md:w-auto">
+          <img
+            className="h-full w-full object-cover"
+            src={img_convo}
+            alt="image of a conversation"
+          />
         </div>
 
-        <div className='w-full md:w-auto p-8'>
-          <h1 className='max-w-xl text-3xl md:text-4xl text-left font-bold font-alegreya text-dark m-3'>
+        <div className="w-full p-8 md:w-auto">
+          <h1 className="m-3 max-w-xl text-left font-alegreya text-3xl font-bold text-dark md:text-4xl">
             VISION
           </h1>
-          <p className='max-w-xl text-left text-dark m-3'>
+          <p className="m-3 max-w-xl text-left text-dark">
             At Home Care Concierge, we believe in the value of...
           </p>
-          <p className='max-w-xl text-left text-dark m-3'>
+          <p className="m-3 max-w-xl text-left text-dark">
             Every family member deserves to be taken care of. We promise to
             treat everyone like family.
           </p>
-          <p className='max-w-xl text-left text-dark m-3'>
+          <p className="m-3 max-w-xl text-left text-dark">
             That's what we're all about.
           </p>
         </div>
@@ -85,37 +87,35 @@ function App() {
           //  MM  MM  MM  MM    MM  MM    MM    MM    
           //    MM  MM    MM    MM  MM    MM    MM     */}
 
-      <section className='relative items-center justify-center p-12'>
+      <section className="relative items-center justify-center p-12">
         <img
-          className='absolute inset-0 object-cover h-full w-full opacity-20 -z-10'
+          className="absolute inset-0 -z-10 h-full w-full object-cover opacity-20"
           src={img_line_wave}
-          alt='image of a house'
+          alt="image of a house"
         />
-        <div className='relative grid grid-cols-1 lg:grid-cols-2 items-center justify-center'>
-        <div className='p-10 md:p-0 w-full md:w-auto '>
-          <div className='rounded-full aspect-square bg-blue-500 overflow-hidden'>
-            <img
-              className='object-cover w-full h-full'
-              src={img_stare}
-              alt='image of two people staring at each other'
-            />
+        <div className="relative grid grid-cols-1 items-center justify-center lg:grid-cols-2">
+          <div className="w-full p-10 md:w-auto md:p-0 ">
+            <div className="aspect-square overflow-hidden rounded-full bg-blue-500">
+              <img
+                className="h-full w-full object-cover"
+                src={img_stare}
+                alt="image of two people staring at each other"
+              />
+            </div>
+          </div>
+          <div className="w-full p-8 md:w-auto lg:order-first ">
+            <h1 className="m-3 max-w-xl text-left font-alegreya text-3xl font-bold text-dark md:text-4xl">
+              WHAT WE DO
+            </h1>
+            <p className="m-3 max-w-xl text-left text-dark">
+              At HOME CARE, we believe that home care is everything. We will
+              treat you like family, because at Home Care, you are family.
+            </p>
+            <p className="m-3 max-w-xl text-left text-dark">
+              We serve homes in Whittier, La Mirada, and Fullerton.
+            </p>
           </div>
         </div>
-        <div className='w-full lg:order-first md:w-auto p-8 '>
-          <h1 className='max-w-xl text-3xl md:text-4xl text-left font-bold font-alegreya text-dark m-3'>
-            WHAT WE DO
-          </h1>
-          <p className='max-w-xl text-left text-dark m-3'>
-            At HOME CARE, we believe that home care is everything. We will treat
-            you like family, because at Home Care, you are family.
-          </p>
-          <p className='max-w-xl text-left text-dark m-3'>
-            We serve homes in Whittier, La Mirada, and Fullerton.
-          </p>
-        </div>
-        </div>
-
-        
       </section>
 
       {/* //  MMMMMM  MMMMMMMM    MMMM    MMMMMM  MMMMMM  MM      MM  
@@ -125,24 +125,24 @@ function App() {
           //    MM    MM        MM    MM    MM      MM    MM      MM  
           //    MM    MMMMMMMM    MMMM      MM    MMMMMM  MM      MM   */}
 
-      <section className='grid grid-cols-1 lg:grid-cols-2 items-center justify-center p-12 bg-gradient-to-r from-sky-200 from-5% via-sky-300 via-50% to-sky-400 to-95%'>
-        <div className='p-10 md:p-0 w-full md:w-auto'>
-          <div className='rounded-full aspect-square bg-blue-500 overflow-hidden'>
+      <section className="grid grid-cols-1 items-center justify-center bg-gradient-to-r from-sky-200 from-5% via-sky-300 via-50% to-sky-400 to-95% p-12 lg:grid-cols-2">
+        <div className="w-full p-10 md:w-auto md:p-0">
+          <div className="aspect-square overflow-hidden rounded-full bg-blue-500">
             <img
-              className='object-cover w-full h-full'
+              className="h-full w-full object-cover"
               src={img_chile}
-              alt='image of man holding a chile'
+              alt="image of man holding a chile"
             />
           </div>
         </div>
-        <div className='w-full md:w-auto p-8'>
-          <h1 className='text-3xl md:text-4xl text-left font-bold font-alegreya text-dark m-3'>
+        <div className="w-full p-8 md:w-auto">
+          <h1 className="m-3 text-left font-alegreya text-3xl font-bold text-dark md:text-4xl">
             TESTIMONIALS
           </h1>
-          <p className='max-w-xl text-left text-dark m-3'>
+          <p className="m-3 max-w-xl text-left text-dark">
             "This was the best thing that happened to me ever!"
           </p>
-          <p className='max-w-xl text-right text-dark m-3'>- John Smith</p>
+          <p className="m-3 max-w-xl text-right text-dark">- John Smith</p>
         </div>
       </section>
 
@@ -153,27 +153,27 @@ function App() {
           //  MM    MM  MM    MM  MM    MM    MM    MM    MM  MM    MM    MM    
           //    MMMM      MMMM    MM    MM    MM    MM    MM    MMMM      MM     */}
 
-      <section className='relative flex flex-row items-center justify-center p-24'>
+      <section className="relative flex flex-row items-center justify-center p-24">
         <img
-          className='absolute inset-0 object-cover h-full w-full opacity-50 -z-10'
+          className="absolute inset-0 -z-10 h-full w-full object-cover opacity-50"
           src={img_abstract}
-          alt='image of a house'
+          alt="image of a house"
         />
-        <div className='p-8'>
-          <h1 className='max-w-xl text-3xl md:text-4xl text-center font-bold font-alegreya text-dark m-3'>
+        <div className="p-8">
+          <h1 className="m-3 max-w-xl text-center font-alegreya text-3xl font-bold text-dark md:text-4xl">
             CONTACT
           </h1>
-          <p className='max-w-xl text-center text-dark m-3'>714 123 4566</p>
-          <p className='max-w-xl text-center text-dark m-3'>
+          <p className="m-3 max-w-xl text-center text-dark">714 123 4566</p>
+          <p className="m-3 max-w-xl text-center text-dark">
             1600 Pennsylvania Avenue NW, Washington, DC 20500
           </p>
-          <p className='max-w-xl text-center text-dark m-3'>
+          <p className="m-3 max-w-xl text-center text-dark">
             not_a_real_email@gmial.com
           </p>
         </div>
       </section>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
