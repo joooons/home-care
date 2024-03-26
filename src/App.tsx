bd91d70a72e7c6ba554bf8c4ff0bd62dcae7706e
joooons <joon.s.chang@gmail.com>
2024-03-26 01:14:13 -0700

    text style updates

--- src/App.tsx
+++ src/App.tsx
@@ -94,9 +94,9 @@ function App() {
 
         <section
           id="vision"
-          className="bg-wave grid grid-rows-2 items-center justify-center lg:grid-cols-2"
+          className="bg-wave lg: grid grid-cols-2 grid-rows-2 items-center justify-center lg:grid-rows-1"
         >
-          <div className="w-full overflow-hidden  p-4 md:w-auto">
+          <div className="w-full overflow-hidden p-4 md:w-auto">
             <img
               className="h-full w-full rounded-2xl object-cover shadow-lg"
               src={img_convo}
@@ -105,13 +105,13 @@ function App() {
           </div>
 
           <div className="flex w-full flex-col justify-center gap-6 px-16 md:w-auto">
-            <h1 className="text-left text-3xl font-bold text-light md:text-4xl">
+            <h1 className="max-w-xl text-center text-3xl font-bold text-light [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)] md:text-4xl lg:text-left lg:text-6xl">
               VISION
             </h1>
-            <div className="text-light">
-              <p className="max-w-xl text-left text-light">We believe...</p>
-              <p className="max-w-xl text-left text-light">We promise...</p>
-              <p className="max-w-xl text-left text-light">We provide...</p>
+            <div className="text-left text-xl font-medium text-white/70 ">
+              <p className="max-w-xl">We believe...</p>
+              <p className="max-w-xl">We promise...</p>
+              <p className="max-w-xl ">We provide...</p>
             </div>
           </div>
         </section>
@@ -148,7 +148,7 @@ function App() {
             </svg>
           </div>
 
-          <div className="relative grid grid-rows-2 items-center justify-center gap-16 p-16 lg:grid-rows-1 lg:flex-row lg:gap-0">
+          <div className="relative grid grid-rows-2 items-center justify-center gap-16 p-16 lg:grid-cols-2 lg:grid-rows-1 lg:gap-0">
             <div className="w-full px-0 sm:px-16 md:px-32">
               <div className="aspect-square overflow-hidden rounded-full bg-blue-500 shadow-lg ">
                 <img
