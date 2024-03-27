d794b8295b6ce98af6df014289c2fe0c5f220d2e
joooons <joon.s.chang@gmail.com>
2024-03-26 22:40:39 -0700

    add blob to ContactUs

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,16 +1,13 @@
-// import img_abstract from "./assets/abstract.jpg"
-import img_abstract from "../assets/abstract.jpg"
+import SvgBlob from "./SvgBlob"
 
 export default function ContactUs() {
   return (
     <>
-      <section id="contact" className="relative ">
-        <img
-          className="absolute inset-0 -z-10 h-full w-full object-cover opacity-50"
-          src={img_abstract}
-          alt="image of abstract paper texture"
-        />
-        <div className="container mx-auto flex flex-col gap-8 px-6 py-24 md:py-32 lg:gap-16 lg:py-40">
+      <section id="contact" className="relative overflow-hidden">
+        <div className="absolute inset-5 -z-10 flex h-full w-full flex-col justify-end object-cover">
+          <SvgBlob></SvgBlob>
+        </div>
+        <div className="container relative mx-auto flex flex-col gap-8 px-6 py-24 md:py-32 lg:gap-16 lg:py-40 xl:py-52">
           <h1 className="max-w-xl text-left text-3xl font-bold text-purp [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)] md:text-4xl lg:text-6xl">
             CONTACT US
           </h1>
