9c5058f74b227fea2f5eb09e91a243f1f61f016e
joooons <joon.s.chang@gmail.com>
2024-03-27 16:02:51 -0700

    refine breakpoint behavior of Vision

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -3,26 +3,31 @@ import img_convo from "../assets/convo.jpg"
 export default function Vision() {
   return (
     <>
-      <section
-        id="vision"
-        className="lg: grid grid-cols-2 grid-rows-2 items-center justify-center bg-prime lg:grid-rows-1"
-      >
-        <div className="w-full overflow-hidden p-4 md:w-auto">
-          <img
-            className="h-full w-full rounded-2xl object-cover shadow-lg"
-            src={img_convo}
-            alt="image of a conversation"
-          />
-        </div>
+      <section id="vision" className="bg-prime py-12 md:py-16 xl:py-24">
+        <div className="container mx-auto flex flex-col gap-6 px-6 md:flex-row ">
+          <div className="overflow-hidden md:w-1/2 xl:mx-10">
+            <img
+              className="h-full w-full rounded-2xl object-cover shadow-lg"
+              src={img_convo}
+              alt="image of a conversation"
+            />
+          </div>
 
-        <div className="flex w-full flex-col justify-center gap-6 px-16 md:w-auto">
-          <h1 className="max-w-xl text-center text-3xl font-bold text-light [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)] md:text-4xl lg:text-left lg:text-6xl">
-            VISION
-          </h1>
-          <div className="text-left text-xl font-medium text-white/70 ">
-            <p className="max-w-xl">We believe...</p>
-            <p className="max-w-xl">We promise...</p>
-            <p className="max-w-xl ">We provide...</p>
+          <div className="flex flex-col justify-center space-y-6 whitespace-nowrap md:w-1/2 xl:mx-10">
+            <h1 className="text-left text-3xl font-bold text-light [text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)] sm:text-center sm:text-4xl md:text-6xl lg:text-6xl">
+              VISION
+            </h1>
+            <div className="text-left text-xl font-medium text-white/70 sm:text-center ">
+              <p className="whitespace-normal">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <p className="whitespace-normal">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+              <p className="whitespace-normal">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
           </div>
         </div>
       </section>
