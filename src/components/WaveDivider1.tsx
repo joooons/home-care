e63751dcf4fa5f3cd5089382e09550223d159fdb
joooons <joon.s.chang@gmail.com>
2024-03-27 14:35:47 -0700

    modularize wave dividers

---
+++ src/components/WaveDivider1.tsx
@@ -0,0 +1,22 @@
+export default function WaveDivider1() {
+  return (
+    <>
+      <div className="-z-5 absolute bottom-[0vh] flex w-full flex-col justify-end">
+        <svg
+          id="visual"
+          viewBox="0 0 900 200"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          className="fill-prime"
+        >
+          <path
+            d="M0 166L21.5 154.7C43 143.3 86 120.7 128.8 109.7C171.7 98.7 214.3 99.3 257.2 112.2C300 125 343 150 385.8 149C428.7 148 471.3 121 514.2 106.3C557 91.7 600 89.3 642.8 101C685.7 112.7 728.3 138.3 771.2 139.2C814 140 857 116 878.5 104L900 92L900 201L878.5 201C857 201 814 201 771.2 201C728.3 201 685.7 201 642.8 201C600 201 557 201 514.2 201C471.3 201 428.7 201 385.8 201C343 201 300 201 257.2 201C214.3 201 171.7 201 128.8 201C86 201 43 201 21.5 201L0 201Z"
+            // fill="#4992ff"
+            strokeLinecap="round"
+            strokeLinejoin="miter"
+          ></path>
+        </svg>
+      </div>
+    </>
+  )
+}
--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -1,3 +1,5 @@
+import WaveDivider2 from "./WaveDivider2"
+
 import img_stare from "../assets/stare.jpg"
 import img_line_wave from "../assets/line-wave.jpg"
 
@@ -13,21 +15,8 @@ export default function WhatWeDo() {
           src={img_line_wave}
           alt="image of wave line art"
         />
-        <div className="absolute -z-10 flex h-full w-full flex-col justify-end">
-          <svg
-            id="visual"
-            viewBox="0 0 900 200"
-            xmlns="http://www.w3.org/2000/svg"
-            version="1.1"
-            className="fill-second"
-          >
-            <path
-              d="M0 124L21.5 123.2C43 122.3 86 120.7 128.8 122.8C171.7 125 214.3 131 257.2 138C300 145 343 153 385.8 150.8C428.7 148.7 471.3 136.3 514.2 139.8C557 143.3 600 162.7 642.8 173.2C685.7 183.7 728.3 185.3 771.2 185.7C814 186 857 185 878.5 184.5L900 184L900 201L878.5 201C857 201 814 201 771.2 201C728.3 201 685.7 201 642.8 201C600 201 557 201 514.2 201C471.3 201 428.7 201 385.8 201C343 201 300 201 257.2 201C214.3 201 171.7 201 128.8 201C86 201 43 201 21.5 201L0 201Z"
-              strokeLinecap="round"
-              strokeLinejoin="miter"
-            ></path>
-          </svg>
-        </div>
+
+        <WaveDivider2></WaveDivider2>
 
         <div className="relative grid grid-rows-2 items-center justify-center gap-16 p-16 lg:grid-cols-2 lg:grid-rows-1 lg:gap-0">
           <div className="w-full px-0 sm:px-16 md:px-32">
