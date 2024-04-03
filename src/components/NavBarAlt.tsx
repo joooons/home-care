f34b56d24de41db7039d0fc19095af9656312903
joooons <joon.s.chang@gmail.com>
2024-04-03 00:37:21 -0700

    try diff navbar style

--- src/components/NavBarAlt.tsx
+++ src/components/NavBarAlt.tsx
@@ -22,34 +22,33 @@ export default function NavBarAlt() {
   }
 
   return (
-    <Disclosure as="nav" className="absolute z-10 w-full bg-black/30">
+    <Disclosure as="nav" className="absolute z-10 w-full">
       {({ open }) => (
         <>
-          <div className="absolute h-24 w-full"></div>
+          <div className="absolute h-24 w-full bg-second/30"></div>
 
           <div className="container mx-auto px-6">
             <div className="relative z-10 flex h-24 items-center justify-between">
-              <div className="focus-visible:ring-purp-50/0 relative inline-flex cursor-pointer items-center justify-center rounded-md p-2 text-light hover:bg-white/50 focus:outline-none focus-visible:ring">
+              <div className="focus-visible:ring-purp-50/0 relative inline-flex cursor-pointer items-center justify-center rounded-md p-2 text-light hover:animate-pulse focus:outline-none focus-visible:ring ">
                 <PhoneIcon
-                  className="block h-8 w-8 stroke-1"
+                  className="stroke-4 block h-8 w-8"
                   aria-hidden="true"
                 />
               </div>
               <div className="flex flex-1 items-center justify-center">
                 {/* Brand Name */}
-                <div className="flex flex-shrink-0 items-center">
-                  <div className="flex flex-row justify-center gap-2 bg-clip-text py-3 font-merriweather text-xl font-medium text-light sm:text-2xl lg:text-3xl">
-                    <span>Home Care</span>
-                    <SvgBird color={"#FFFFFF"}></SvgBird>
-                    <span>Consultants</span>
-                  </div>
+
+                <div className="flex flex-row justify-center gap-2 bg-clip-text py-3 font-merriweather text-xl font-medium text-light sm:text-2xl lg:text-3xl">
+                  <span className="hidden md:block">Home Care</span>
+                  <SvgBird color={"#FFFFFF"}></SvgBird>
+                  <span className="hidden md:block">Consultants</span>
                 </div>
 
                 {/* Desktop Menu Items */}
               </div>
               <div className="inset-y-0 left-0 flex items-center">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="focus-visible:ring-purp-50/0 relative inline-flex items-center justify-center rounded-md p-2 text-light hover:bg-white/50 focus:outline-none focus-visible:ring">
+                <Disclosure.Button className="focus-visible:ring-purp-50/0 relative inline-flex items-center justify-center rounded-md p-2 text-light hover:animate-pulse focus:outline-none focus-visible:ring">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -70,21 +69,23 @@ export default function NavBarAlt() {
 
           {/* Mobile Menu Items */}
           <Disclosure.Panel>
-            <ul className="flex flex-col justify-between gap-2 space-y-1 bg-black/90 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="li"
-                  className={classNames(
-                    "block cursor-pointer rounded-md px-3 py-4 text-2xl text-light hover:bg-white/30 hover:text-light"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                  onClick={() => handleClick(item.scrollto)}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </ul>
+            <div className="bg-dark pb-8">
+              <ul className="flex flex-col justify-between gap-2 space-y-1 px-2 pb-3 pt-2">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="li"
+                    className={classNames(
+                      "block cursor-pointer rounded-md px-3 py-4 text-2xl text-light  hover:animate-pulse hover:[text-shadow:_0px_0px_25px_rgb(255_255_255_/_70%)]"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                    onClick={() => handleClick(item.scrollto)}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </ul>
+            </div>
           </Disclosure.Panel>
         </>
       )}
