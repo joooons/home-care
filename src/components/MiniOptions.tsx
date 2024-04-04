7c735203f3edcff2cd9c56aa91ac2a1cb42a5882
joooons <joon.s.chang@gmail.com>
2024-04-03 17:40:03 -0700

    make minioptions navigate both ways

--- src/components/MiniOptions.tsx
+++ src/components/MiniOptions.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom"
 
-export default function MiniOptions() {
+interface Props {
+  text?: string
+  link?: string
+}
+
+export default function MiniOptions({ text = "ALT", link = "/alt" }: Props) {
   return (
     <>
       <div className="fixed bottom-0 left-0 m-5">
-        <Link to="/alt">
+        <Link to={link}>
           <button className="focus-visible cursor-pointer rounded-full border-4 bg-prime px-6 py-3 font-medium text-light opacity-10 transition hover:opacity-100">
-            ALT
+            {text}
           </button>
         </Link>
       </div>
