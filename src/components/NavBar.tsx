de4a1f1a8c93894f6210737e9c041dc62b390aa6
joooons <joon.s.chang@gmail.com>
2024-04-03 17:32:54 -0700

    load google font options

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -31,7 +31,7 @@ export default function NavBar() {
                 {/* Brand Name */}
                 <div className="flex flex-shrink-0 items-center">
                   {/* <img className='h-8 w-auto' src={icon} alt='Home Care'/> */}
-                  <div className="bg-clip-text py-3 font-merriweather text-xl font-medium text-transparent text-white sm:text-2xl lg:text-3xl">
+                  <div className="bg-clip-text py-3 text-xl font-medium text-transparent text-white sm:text-2xl lg:text-3xl">
                     The Home Care Consultants
                   </div>
                 </div>
