c57af02e702eb49bdd9a97b25716e31f84f02284
joooons <joon.s.chang@gmail.com>
2024-04-04 12:34:32 -0700

    center text in WhatWeDo

--- src/components/WhatWeDoAlt.tsx
+++ src/components/WhatWeDoAlt.tsx
@@ -13,9 +13,43 @@ import {
 import img_stare from "../assets/stare.jpg"
 import img_line_wave from "../assets/line-wave.jpg"
 
-export default function WhatWeDoAlt() {
-  const iconColor = "#D4A373"
+const iconColor = "#D4A373"
+// const iconColor = "#237AFC"
 
+const content = [
+  {
+    image: (
+      <HeartIcon
+        className="h-8 w-8 stroke-2"
+        aria-hidden="true"
+        color={iconColor}
+      />
+    ),
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptatum earum animi non nesciunt. Est reiciendis auteius unde doloribus necessitatibus consectetur atque aspernatur autem, debitis neque facere suscipit laborum. ",
+  },
+  {
+    image: (
+      <HomeIcon
+        className="h-8 w-8 stroke-2"
+        aria-hidden="true"
+        color={iconColor}
+      />
+    ),
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptatum earum animi non nesciunt. Est reiciendis auteius unde doloribus necessitatibus consectetur atque aspernatur autem, debitis neque facere suscipit laborum. ",
+  },
+  {
+    image: (
+      <TrophyIcon
+        className="h-8 w-8 stroke-2"
+        aria-hidden="true"
+        color={iconColor}
+      />
+    ),
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptatum earum animi non nesciunt. Est reiciendis auteius unde doloribus necessitatibus consectetur atque aspernatur autem, debitis neque facere suscipit laborum. ",
+  },
+]
+
+export default function WhatWeDoAlt() {
   return (
     <>
       <section
@@ -30,7 +64,7 @@ export default function WhatWeDoAlt() {
 
         {/* <WaveDivider2></WaveDivider2> */}
 
-        <div className="container relative mx-auto flex flex-col items-center justify-center gap-8 p-16 px-6 pb-20 md:flex-row lg:gap-0">
+        <div className="container relative mx-auto flex flex-col items-center justify-center gap-8 p-6 pb-20 md:flex-row lg:gap-0 lg:py-16">
           <div className="w-full sm:px-16 xl:mx-10">
             <div className="aspect-square overflow-hidden rounded-full shadow-lg ">
               <img
@@ -41,51 +75,20 @@ export default function WhatWeDoAlt() {
             </div>
           </div>
           <div className="flex w-full flex-col gap-6 md:order-first xl:mx-10">
-            <h1 className="font-libre max-w-xl text-left text-3xl font-bold text-tan lg:text-6xl dark:text-light">
+            <h1 className="max-w-xl text-center font-libre text-3xl font-bold text-tan lg:text-6xl dark:text-light">
               What We Do
             </h1>
-            <div className="font-newsreader flex flex-col gap-4 text-left text-xl font-medium text-dark/60 lg:text-xl dark:text-light/60">
-              <p>
-                <span className="flex justify-center">
-                  <HeartIcon
-                    className="h-8 w-8 stroke-2"
-                    aria-hidden="true"
-                    color={iconColor}
-                  />
-                </span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Expedita voluptatum earum animi non nesciunt. Est reiciendis aut
-                eius unde doloribus necessitatibus consectetur atque aspernatur
-                autem, debitis neque facere suscipit laborum.
-              </p>
-
-              <p>
-                <span className="flex justify-center">
-                  <HomeIcon
-                    className="h-8 w-8 stroke-2"
-                    aria-hidden="true"
-                    color={iconColor}
-                  />
-                </span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Expedita voluptatum earum animi non nesciunt. Est reiciendis aut
-                eius unde doloribus necessitatibus consectetur atque aspernatur
-                autem, debitis neque facere suscipit laborum.
-              </p>
-
-              <p>
-                <span className="flex justify-center">
-                  <TrophyIcon
-                    className="h-8 w-8 stroke-2"
-                    aria-hidden="true"
-                    color={iconColor}
-                  />
-                </span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Expedita voluptatum earum animi non nesciunt. Est reiciendis aut
-                eius unde doloribus necessitatibus consectetur atque aspernatur
-                autem, debitis neque facere suscipit laborum.
-              </p>
+            <div className="flex flex-col gap-4 text-left font-newsreader text-xl font-medium text-dark/60 lg:text-xl dark:text-light/60">
+              {content.map((item) => {
+                return (
+                  <>
+                    <p>
+                      <span className="flex justify-center">{item.image}</span>
+                      <div className="text-center">{item.text}</div>
+                    </p>
+                  </>
+                )
+              })}
             </div>
           </div>
         </div>
