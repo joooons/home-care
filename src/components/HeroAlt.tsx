108d20702ee720c0d1f53da2fea3246c833a109c
joooons <joon.s.chang@gmail.com>
2024-04-04 12:11:05 -0700

    change photos used in carousel

--- src/components/HeroAlt.tsx
+++ src/components/HeroAlt.tsx
@@ -7,14 +7,14 @@ export default function HeroAlt() {
 
   return (
     <>
-      <section className="relative h-screen w-full items-center justify-center  bg-black/85">
+      <section className="relative h-screen w-full items-center justify-center bg-black/80">
         <Carousel></Carousel>
         <div className="container mx-auto flex h-full flex-col px-6 py-32 xl:pb-56">
           <div className="flex h-full flex-col items-center justify-center space-y-6 ">
-            <h1 className="font-libre text-center text-3xl font-bold text-tan md:text-4xl lg:text-6xl dark:text-light">
+            <h1 className="text-center font-libre text-3xl font-bold text-tan md:text-4xl lg:text-6xl dark:text-light">
               We <span className="text-prime ">care</span> for your loved ones
             </h1>
-            <p className="font-newsreader max-w-xl text-center text-xl font-medium text-light/80 dark:text-light/60">
+            <p className="max-w-xl text-center font-newsreader text-xl font-medium text-light/80 dark:text-light/60">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Perferendis mollitia vero vel quo cupiditate sint quis laborum
               accusantium velit eos, pariatur necessitatibus, provident amet
